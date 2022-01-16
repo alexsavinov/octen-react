6c837f20d8408f595b0410c311e56543d0e7fbfa
alexsavinov <spell477@gmail.com>
2022-01-16 03:53:54 +0200

    react - lesson01 - cw

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 
+// есть API от SpaceX
+// https://api.spacexdata.com/v3/launches/
+// нужно вывести все запуски кораблей кроме 2020 года.
+// информация нужна не вся, только mission_name, launch_year  и mission_patch_small:
 function App() {
     return (
         <div className="App">
