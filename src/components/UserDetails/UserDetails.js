dac6b4fd03059c8d5b0cc6deb1d1ba87c9e2329c
alexsavinov <spell477@gmail.com>
2022-01-20 10:46:28 +0200

    react - lesson02 - hw

--- src/components/UserDetails/UserDetails.js
+++ src/components/UserDetails/UserDetails.js
@@ -3,7 +3,7 @@ import React from 'react';
 import "./UserDetails.css";
 
 const UserDetails = ({user, getUserId}) => {
-    const {id, name, username, email, address} = user;
+    const {id, name, username, email, address, phone, website, company} = user;
 
     return (
         <div className="userdetails">
@@ -12,7 +12,22 @@ const UserDetails = ({user, getUserId}) => {
             <div>name: {name}</div>
             <div>username: {username}</div>
             <div>email: {email}</div>
-            <div>street: {address.street}</div>
+            <div className="block">
+                <b>Address</b>
+                <div>street: {address.street}</div>
+                <div>suite: {address.suite}</div>
+                <div>city: {address.city}</div>
+                <div>zipcode: {address.zipcode}</div>
+                <div>geo: {address.geo.lat}. {address.geo.lng}</div>
+            </div>
+            <div>phone: {phone}</div>
+            <div>website: {website}</div>
+            <div className="block">
+                <b>Company</b>
+                <div>name: {company.street}</div>
+                <div>catchPhrase: {company.catchPhrase}</div>
+                <div>bs: {company.bs}</div>
+            </div>
             <button onClick={() => getUserId(id)}>Posts</button>
         </div>);
 };
